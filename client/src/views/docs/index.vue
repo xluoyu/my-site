1e7a0613bd33afa2bbad57e49f6f0a6bac117407
xluoyu <llzz0895@163.com>
2022-03-17 13:53:32 +0800

    feat: 完善docs功能

--- client/src/views/docs/index.vue
+++ client/src/views/docs/index.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>
-
+  <div class="w-400px bg-light-50 m-auto" v-html="docs">
   </div>
 </template>
 
 <script setup lang="ts">
+  import { GetTestDocs } from '@/api/docs'
+  const docs = ref('')
+  onMounted(() => {
+    GetTestDocs().then(res => {
+      docs.value = res.data.html
+
+      console.log(docs.value)
+    })
+  })
 
 </script>
 
