44b14923a8f64cdc0030b5f01cb9255294c05a74
xluoyu <llzz0895@163.com>
2022-03-25 09:50:37 +0800

    feat: remove server

--- server/src/modules/docs/dto/update-doc.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateDocDto } from './create-doc.dto';
-
-export class UpdateDocDto extends PartialType(CreateDocDto) {}
--- server/src/modules/music/music.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MusicService } from './music.service';
-import { MusicController } from './music.controller';
-
-@Module({
-  controllers: [MusicController],
-  providers: [MusicService]
-})
-export class MusicModule {}
