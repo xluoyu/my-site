fd6e458ccfb353c017291b0ff8446820849d7051
xluoyu <llzz0895@163.com>
2022-03-31 16:34:06 +0800

    add music

---
+++ src/packages/music/components/Aside.vue
@@ -0,0 +1,13 @@
+<template>
+  <aside>
+    左侧菜单栏
+  </aside>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- src/packages/music/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Music from './index.vue'
-
-describe('music test', () => {
-  it('mount component', async() => {
-    const component = mount(Music)
-    expect(component.text()).toMatch('音乐播放器')
-  })
-})
